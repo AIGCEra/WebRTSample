30d54868eafb611aaf642fd33585d3f543be5491
tangram <tangramteam@outlook.com>
2022-12-27 07:35:52 +0800

    update sample

--- Sample/DotnetComponent/SunnyForms2/Form4.Designer.cs
+++ Sample/DotnetComponent/SunnyForms2/Form4.Designer.cs
@@ -38,36 +38,40 @@
             this.mainpanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.mainpanel.Location = new System.Drawing.Point(87, 153);
+            this.mainpanel.Location = new System.Drawing.Point(44, 73);
+            this.mainpanel.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.mainpanel.Name = "mainpanel";
-            this.mainpanel.Size = new System.Drawing.Size(632, 238);
+            this.mainpanel.Size = new System.Drawing.Size(518, 238);
             this.mainpanel.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(82, 59);
+            this.button1.Location = new System.Drawing.Point(41, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(165, 60);
+            this.button1.Size = new System.Drawing.Size(82, 29);
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(320, 61);
+            this.textBox1.Location = new System.Drawing.Point(160, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 31);
+            this.textBox1.Size = new System.Drawing.Size(378, 21);
             this.textBox1.TabIndex = 4;
             // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(602, 340);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.mainpanel);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "Form4";
             this.Text = "Form4";
             this.Load += new System.EventHandler(this.Form4_Load);
